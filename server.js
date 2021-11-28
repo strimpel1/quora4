bba3cadfa1ef71995424b003f7830b2333116c92
strimpel1 <strimpel3@gmail.com>
2021-11-28 12:42:05 +0200

    add all

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-// Load Node modules
-var express = require('express');
-// Initialise Express
-var app = express();
-// Render static files
-app.use(express.static('public/'));
-// Port website will run on
-app.listen(80);
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
